be353c87176ac7b3735ef59709767b87381a8da9
Chang-Yen-Cheng <56288695+Chang-Yen-Cheng@users.noreply.github.com>
2021-06-30 18:08:19 +0100

    Update sswm.js

--- sswm.js
+++ sswm.js
@@ -265,9 +265,7 @@ trial_duration: 1000
 
 var p_details = {
 type: 'html-keyboard-response',         
-stimulus: function(){
-  return '<div style="font-size:20px;">Welcome!.<br><br>The task will begin in few seconds.</div>'
-},
+stimulus: " Welcome to the experiment",
 choices: jsPsych.NO_KEYS,
 trial_duration: 1000
 }
